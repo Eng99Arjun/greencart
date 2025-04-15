d703558d213e82890d9e825a1e001aff25aecdab
Eng99Arjun <dhananjay.99jb@gmail.com>
2025-04-15 19:25:59 +0530

    feat: Add ProductCategory page and routing for product categories

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Footer from './components/Footer'
 import { useAppContext } from './context/AppContext'
 import Login from './components/Login'
 import AllProducts from './pages/AllProducts'
+import ProductCategory from './pages/ProductCategory'
 
 const App = () => {
 
@@ -29,6 +30,7 @@ const App = () => {
           
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<AllProducts />} />
+          <Route path="/products/:category" element={<ProductCategory />} />
         </Routes>
       </div>
       {!isSellerPath && <Footer/> }
