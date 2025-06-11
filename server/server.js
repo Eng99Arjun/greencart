61631c710223df190df3934f7f07cc5d775729f6
Eng99Arjun <dhananjay.99jb@gmail.com>
2025-06-11 11:01:45 +0530

    feat: update allowed origins to include Vercel deployment URL

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ await connectCloudinary()
 
 
 //Allow multiple origins
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173', 'https://greencart-orpin.vercel.app']
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebHooks)
 
